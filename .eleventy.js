93446e7ee4a6459e929c7ea5476816b5dbc23373
Graeme McIntosh <2190988+jugR0jones@users.noreply.github.com>
2025-12-18 06:03:52 +0200

    Add global stylesheet and configure Eleventy to copy assets folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 ﻿module.exports = function(eleventyConfig) {
+  // Copy the assets folder (css, images, etc.) from the input directory to the output
+  // Use an explicit mapping to ensure copy works correctly with custom input dir
+  eleventyConfig.addPassthroughCopy({ "site/assets": "assets" });
+
   return {
     dir: {
       input: "site",
